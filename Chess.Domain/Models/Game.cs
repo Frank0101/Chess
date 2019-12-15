91f1562417b050ee45acc8ec759f473132126dae
Francesco Cossu <francesco.cossu@gmail.com>
2019-12-15 14:20:53 +0000

    Refactoring

--- Chess.Domain/Models/Game.cs
+++ Chess.Domain/Models/Game.cs
@@ -7,18 +7,18 @@ namespace Chess.Domain.Models
     public class Game
     {
         public Board Board { get; }
-        public PiecesColor TurnColor { get; set; }
         public IPlayer WhitePlayer { get; }
         public IPlayer BlackPlayer { get; }
         public Action<Board, PiecesColor> OnNewTurn { get; }
+        public PiecesColor TurnColor { get; set; }
 
         public Game(IPlayer whitePlayer, IPlayer blackPlayer, Action<Board, PiecesColor> onNewTurn)
         {
             Board = new Board();
-            TurnColor = PiecesColor.White;
             WhitePlayer = whitePlayer;
             BlackPlayer = blackPlayer;
             OnNewTurn = onNewTurn;
+            TurnColor = PiecesColor.White;
         }
     }
 }
